fe2069f52c099ad6bb89ae1cc204dbc5c66d85cc
nadee <nzzz101.3z@gmail.com>
2025-06-21 13:01:55 +0530

    First Public Release

---
+++ tag_release.bat
@@ -0,0 +1,13 @@
+# Check your current branch
+git branch
+
+# See if your tag exists locally and remotely
+git tag
+
+# Delete tag locally and remotely if necessary
+git tag -d v1.1.1
+git push origin :refs/tags/v1.1.1
+
+# Re-create tag at current commit
+git tag -a v1.1.1 -m "First Public Release"
+git push origin v1.1.1
