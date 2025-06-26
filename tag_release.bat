5242ae1ae8915f0a66554b16c1e7e53d65495efb
nadee <nzzz101.3z@gmail.com>
2025-06-26 12:41:48 +0530

    cleanup tag_release.bat

--- tag_release.bat
+++ tag_release.bat
@@ -1,13 +1,9 @@
-# Check your current branch
 git branch
 
-# See if your tag exists locally and remotely
 git tag
 
-# Delete tag locally and remotely if necessary
 git tag -d v1.1.5
 git push origin :refs/tags/v1.1.5
 
-# Re-create tag at current commit
 git tag -a v1.1.5 -m "+ Added space for barcode/ QR separator. "
 git push origin v1.1.5
