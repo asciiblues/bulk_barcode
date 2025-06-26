566b0f63b13655ce5afc83c8ac76a2215fd35796
nadee <nzzz101.3z@gmail.com>
2025-06-26 12:42:23 +0530

    cleanup tag_release.bat

--- tag_release.bat
+++ tag_release.bat
@@ -1,9 +1,13 @@
+# Check your current branch
 git branch
 
+# See if your tag exists locally and remotely
 git tag
 
-git tag -d v1.1.5
-git push origin :refs/tags/v1.1.5
+# Delete tag locally and remotely if necessary
+git tag -d v1.1.4
+git push origin :refs/tags/v1.1.4
 
-git tag -a v1.1.5 -m "+ Added space for barcode/ QR separator. "
-git push origin v1.1.5
+# Re-create tag at current commit
+git tag -a v1.1.4 -m "✓ Fixed 'Use New Line Separator' CheckBoxTile before 'Read Data Column' Button"
+git push origin v1.1.4
